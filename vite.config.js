94eb58a5777bcde677458ec16c9586c52342febe
Joaquin Quiroga <joaquinquiroga22@gmail.com>
2023-12-05 19:55:24 -0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import sassPlugin from 'vite-plugin-sass';
 
 // https://vitejs.dev/config/
 export default defineConfig({
